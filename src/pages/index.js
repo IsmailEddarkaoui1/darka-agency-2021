babedbdead3eb9450008bf981cc196cf93021a2f
Eddarkaoui Ismail (s109205) <ismail.eddarkaoui@student.ap.be>
2021-10-19 13:10:02 +0200

    Gatbsy Plugins finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,21 @@
 // Stap 1: Importeer React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 // Stap 2: definieer je component
 const IndexPage = () => {
   return (
     <main>
       <Layout pageTitle ="Welcome to Darka Agency!">
       <p>Lorem ipsum</p>
+      <StaticImage        
+      alt="OnePiece image!"        
+      src="https://i.pinimg.com/564x/91/de/08/91de0880c76a6523e05761e591735743.jpg"      
+      />
+      <StaticImage
+      alt="Kingdom 3rd season"
+      src="../images/kingdom3.jpg"
+      />
       </Layout>
     </main>
   )
