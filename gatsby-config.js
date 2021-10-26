3a01c3e2a9e60252db75e9b3c4773701d0246940
Eddarkaoui Ismail (s109205) <ismail.eddarkaoui@student.ap.be>
2021-10-26 15:10:57 +0200

    Query data met GraphQL finished

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,16 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
-  ],
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        /*
+         * De volledige URL van je Headless WordPress site's GraphQL API.
+         * Voorbeeld : "https://www.example-site.com/graphql"
+         */
+        url: "http://darka-agency.local/graphql"
+        
+      },
+    },
+  ],  
 };
